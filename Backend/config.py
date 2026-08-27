d6a9f29530d9f226096adc126c00e8a9eca1dbbc
shekshavali1 <shekshav03@gmail.com>
2026-08-27 16:49:46 +0530

    Fix Railway MySQL connection

--- Backend/config.py
+++ Backend/config.py
@@ -4,11 +4,11 @@ import pymysql
 
 def get_connection():
 
-    host = os.environ.get("DB_HOST")
-    user = os.environ.get("DB_USER")
-    password = os.environ.get("DB_PASSWORD")
-    database = os.environ.get("DB_NAME")
-    port = int(os.environ.get("DB_PORT", "3306"))
+    host = os.environ.get("MYSQLHOST")
+    user = os.environ.get("MYSQLUSER")
+    password = os.environ.get("MYSQLPASSWORD")
+    database = os.environ.get("MYSQLDATABASE")
+    port = int(os.environ.get("MYSQLPORT", "3306"))
 
     print("DB_HOST:", host)
     print("DB_USER:", user)
